a941bad975387677d8255b8a3483944defa2423a
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:41:16 +0530

    Added:parttime

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -4,14 +4,19 @@ public class EmployeeJavaProb {
 
 	public static void main(String[] args) {
 		int IS_FULLTIME = 1;
+		int IS_PARTTIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = (Math.floor(Math.random() * 10) % 2);
+		double empCheck = (Math.floor(Math.random() * 10) % 3);
 
 		if (empCheck == IS_FULLTIME) 
 		{
 			empHrs = 8;
+		}
+		else if (empCheck == IS_PARTTIME) 
+		{
+			empHrs = 4;
 		}
 		else 
 		{
