21cc04d238354c0d21df9dcfd3e1083505daff98
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:50:39 +0530

    Added :Added: Calculate Wages till a condition of total working hours of 100 or max days of 20 is reached for a month

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -5,14 +5,15 @@ public class EmployeeJavaProb {
 	public static final int IS_PARTTIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NO_OF_WORKING_DAYS = 20;
-	
+	public static final int MAX_HRS_IN_MONTH = 160;
 	public static void main(String[] args)
 	{
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-	for (int day=0; day<NO_OF_WORKING_DAYS; day++ )
+		int totalempHrs = 0;
+		int totalWorkingDays = 0;
+	while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
 	{
+		totalWorkingDays++;
 		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
 		switch (empCheck)
@@ -26,11 +27,10 @@ public class EmployeeJavaProb {
 		default:
 			empHrs = 0;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		System.out.println("Emp Wage: " + empWage);
+		totalempHrs += empHrs;
+		System.out.println("Day " + totalWorkingDays + "\tEMP HRS: " + empHrs);
 	}
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+	int totalempWage = totalempHrs * EMP_RATE_PER_HOUR;	
+	System.out.println("total Emp Wage: " + totalempWage);
 	}
-
 }
