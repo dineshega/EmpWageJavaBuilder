c3612f18f919f8226dcab6d2dc6a66c56dbd4e80
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 12:21:36 +0530

    Interface implementing

---
+++ src/com/bridgelabz/InterfaceEmpWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public interface InterfaceEmpWage {
+	
+	public void addComEmpWage(String company, int wagePerHour, int numberofWorkingDays, int maxHoursInMonth);
+	
+	public void computeEmpWage();
+
+}
