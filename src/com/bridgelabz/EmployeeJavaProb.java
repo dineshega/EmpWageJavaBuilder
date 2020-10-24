3ccbc8db315431562a3a45cb572037dd4b64aa8e
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 12:03:07 +0530

    Added: Create comp Emp Wage class

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -2,38 +2,40 @@ package com.bridgelabz;
 
 
 public class EmployeeJavaProb {
-	public static final int IS_FULLTIME = 1;
-	public static final int IS_PARTTIME = 2;
-	private int totalempWage;
-	private final String Company;
-	private final int EMP_RATE_PER_HOUR;
-	private final int NO_OF_WORKING_DAYS;
-	private final int MAX_HRS_IN_MONTH;
+	public int numOfCompanies = 0;
 	
-	public EmployeeJavaProb(String cmp, int ERPH, int NOWD, int MHIM)
+	CompanyEmpWage EmpWageArray[];
+	public EmployeeJavaProb() {	
+		EmpWageArray = new CompanyEmpWage[5];
+	}
+	void addComEmpWage(String company, int EMP_RATE_PER_HOUR, int NO_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
 	{
-		this.Company = cmp;
-		this.EMP_RATE_PER_HOUR = ERPH;
-		this.NO_OF_WORKING_DAYS = NOWD;
-		this.MAX_HRS_IN_MONTH = MHIM;
+		EmpWageArray[numOfCompanies] = new CompanyEmpWage(company, EMP_RATE_PER_HOUR, NO_OF_WORKING_DAYS, MAX_HRS_IN_MONTH);
+		numOfCompanies++;
+	}
+	void computeEmpWage() {
+		for (int i = 0; i < numOfCompanies; i++)
+		{
+			EmployeeW(EmpWageArray[i]);
+		}
 	}
 	
-	public void EmployeeW()
+	private void EmployeeW(CompanyEmpWage EmpWage)
 	{
 				int empHrs = 0;
 				int totalempHrs = 0;
 				int totalWorkingDays = 0;
-			while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
+			while (totalempHrs < EmpWage.MAX_HRS_IN_MONTH && totalWorkingDays < EmpWage.NO_OF_WORKING_DAYS)
 			{
 				totalWorkingDays++;
 				int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 		
 				switch (empCheck)
 				{
-				case IS_FULLTIME:
+				case 1:
 					empHrs = 8;
 					break;
-				case IS_PARTTIME:
+				case 2:
 					empHrs = 4;
 					break;
 				default:
@@ -41,17 +43,19 @@ public class EmployeeJavaProb {
 				}
 				totalempHrs += empHrs;
 				System.out.println("Day " + totalWorkingDays + "\tEMP HRS: " + empHrs);
-			}
-			totalempWage = totalempHrs * EMP_RATE_PER_HOUR;	
-			System.out.println("Total Employee Wage for Company " + Company + " is : " + totalempWage);
+			}	
+			int totalempWage =  totalempHrs * EmpWage.EMP_RATE_PER_HOUR;
+			System.out.println("Total Employee Wage for Company " + EmpWage.Company + " is : " + totalempWage);
 	}
 public static void main(String[] args)
 {
-	EmployeeJavaProb TCS = new EmployeeJavaProb("TCS",20,20,160);
-	EmployeeJavaProb HCL=new EmployeeJavaProb("HCL",24,23,150);
-	EmployeeJavaProb Deliotte = new EmployeeJavaProb("Deliotte",28,23,140);
-	TCS.EmployeeW();
-	HCL.EmployeeW();
-	Deliotte.EmployeeW();
+	EmployeeJavaProb cmp=new EmployeeJavaProb();
+	cmp.addComEmpWage("TCS",20,20,160);
+
+	cmp.addComEmpWage("HCL",25,23,120);
+	
+	cmp.addComEmpWage("Deliotte",30,24,110);
+	
+	cmp.computeEmpWage();
 }
 }
