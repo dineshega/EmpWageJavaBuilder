15dcc28cdcb303438ed6fb9b03b75045c3d38e81
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:36:02 +0530

    Added: Check Emp

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -3,7 +3,15 @@ package com.bridgelabz;
 public class EmployeeJavaProb {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to emp wage prob");
+		int IS_FULLTIME = 1;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+
+		if (empCheck == IS_FULLTIME) {
+			System.out.println("Emplyoee is present");
+		}
+		else {
+			System.out.println("Emplyoee is absent");
+		}
 
 	}
 
