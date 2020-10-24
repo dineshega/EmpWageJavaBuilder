a09d10abfe3d704a785a5f8833bae8ac310031e0
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:53:36 +0530

    Added: function

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -6,31 +6,37 @@ public class EmployeeJavaProb {
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NO_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 160;
-	public static void main(String[] args)
+	
+	void EmployeeW()
 	{
-		int empHrs = 0;
-		int totalempHrs = 0;
-		int totalWorkingDays = 0;
-	while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
-	{
-		totalWorkingDays++;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-
-		switch (empCheck)
-		{
-		case IS_FULLTIME:
-			empHrs = 8;
-			break;
-		case IS_PARTTIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
-		}
-		totalempHrs += empHrs;
-		System.out.println("Day " + totalWorkingDays + "\tEMP HRS: " + empHrs);
+				int empHrs = 0;
+				int totalempHrs = 0;
+				int totalWorkingDays = 0;
+			while (totalempHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
+			{
+				totalWorkingDays++;
+				int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+		
+				switch (empCheck)
+				{
+				case IS_FULLTIME:
+					empHrs = 8;
+					break;
+				case IS_PARTTIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+				}
+				totalempHrs += empHrs;
+				System.out.println("Day " + totalWorkingDays + "\tEMP HRS: " + empHrs);
+			}
+			int totalempWage = totalempHrs * EMP_RATE_PER_HOUR;	
+			System.out.println("total Emp Wage: " + totalempWage);
 	}
-	int totalempWage = totalempHrs * EMP_RATE_PER_HOUR;	
-	System.out.println("total Emp Wage: " + totalempWage);
-	}
+public static void main(String[] args)
+{
+	EmployeeJavaProb Euc7=new EmployeeJavaProb();
+			Euc7.EmployeeW();
+}
 }
