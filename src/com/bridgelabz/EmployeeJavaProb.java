6219443c4de07a6753e804c06f6fcc68744174f3
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:46:12 +0530

    Added:Switch case

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -2,28 +2,28 @@ package com.bridgelabz;
 
 public class EmployeeJavaProb {
 
-	public static void main(String[] args) {
-		int IS_FULLTIME = 1;
-		int IS_PARTTIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck = (Math.floor(Math.random() * 10) % 3);
+		public static final int IS_FULLTIME = 1;
+		public static final int IS_PARTTIME = 2;
+		public static final int EMP_RATE_PER_HOUR = 20;
+		public static void main(String[] args)
+		{
+			int empHrs = 0;
+			int empWage = 0;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-		if (empCheck == IS_FULLTIME) 
-		{
-			empHrs = 8;
-		}
-		else if (empCheck == IS_PARTTIME) 
-		{
-			empHrs = 4;
-		}
-		else 
-		{
-			empHrs = 0;
-		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+			switch (empCheck)
+			{
+			case IS_FULLTIME:
+				empHrs = 8;
+				break;
+			case IS_PARTTIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
 	}
 
 }
