4eb3df2b9a67c274db32bafe2442bb2d2440df47
“dineshega” <dinesh15.ega@gmail.com>
2020-10-24 11:39:34 +0530

    Added: Cal Wage

--- src/com/bridgelabz/EmployeeJavaProb.java
+++ src/com/bridgelabz/EmployeeJavaProb.java
@@ -4,15 +4,21 @@ public class EmployeeJavaProb {
 
 	public static void main(String[] args) {
 		int IS_FULLTIME = 1;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = (Math.floor(Math.random() * 10) % 2);
 
-		if (empCheck == IS_FULLTIME) {
-			System.out.println("Emplyoee is present");
+		if (empCheck == IS_FULLTIME) 
+		{
+			empHrs = 8;
 		}
-		else {
-			System.out.println("Emplyoee is absent");
+		else 
+		{
+			empHrs = 0;
 		}
-
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 
 }
